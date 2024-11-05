9f66cd3d603fe15861e7dfd2be2d9154011ec98d
itsnothuy <huytrngqu@gmail.com>
2024-11-04 23:10:52 -0500

    Build the ProfileScreen component

--- src/App.js
+++ src/App.js
@@ -24,23 +24,20 @@
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import { login, logout, selectUser } from './features/userSlice';
-
 import { auth } from './firebase';
-
 import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
-
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((userAuth => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-
         dispatch(login({
           uid: userAuth.uid,
           email: userAuth.email,
@@ -48,21 +45,21 @@ function App() {
       } else {
         dispatch(logout());
       }
-    }));
+    });
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
+
   return (
     <div className="app">
       <Router>
-        {!user ?  
-          <LoginScreen /> 
-          : (
-            <Routes>
-              {/* <Route path="/profile" element={<ProfileScreen />} /> */}
-              <Route path="/" element={<HomeScreen />} />
-            </Routes>
-          )
-        }
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
       </Router>
     </div>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import "./Nav.css";
+import { createBrowserHistory } from "history"
+
 
 const Nav = () => {
     const [show, handleShow] = useState(false);
+    const history = createBrowserHistory();
 
     const transitionNavBar = () => {
         if (window.scrollY > 100) {
@@ -21,11 +24,13 @@ const Nav = () => {
     <div className={`nav ${show && "nav__black"}$`}>
         <div className='nav__contents'>
             <img
+                onClick={() => history.push("/")}
                 className='nav__logo'
                 src='https://loodibee.com/wp-content/uploads/Netflix-logo.png'
                 alt='This is Netflix logo'
             />
             <img 
+                onClick={() => history.push("/profile")}
                 className='nav__avatar'
                 src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
                 alt='This is user avatar logo'
