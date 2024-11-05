3ac9a1d2901b5dca320f0644534ebf29d413ad35
itsnothuy <huytrngqu@gmail.com>
2024-11-04 22:29:06 -0500

    Build the signin/logout functionality

--- src/App.js
+++ src/App.js
@@ -26,24 +26,27 @@ import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React, { useEffect } from 'react';
+import { login, logout, selectUser } from './features/userSlice';
 
 import { auth } from './firebase';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth => {
       if (userAuth) {
 
-        // dispatch(login({
-        //   uid: userAuth.uid,
-        //   email: userAuth.email,
-        // }));
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }));
       } else {
-        // dispatch(logout());
+        dispatch(logout());
       }
     }));
     return unsubscribe;
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../features/userSlice'
+import { auth } from '../firebase'
+import NavBar from '../Navbar'
+import './ProfileScreen.css'
+
+const ProfileScreen = () => {
+    return (
+        <div className='profileScreen'>
+            
+        </div>
+    )
+}
+
+export default ProfileScreen;
